6b634d4047e89b287457eba8b7ee2efcb3b8e07f
sebas7277478 <96531886+sebas7277478@users.noreply.github.com>
2025-05-29 16:59:17 -0500

    Update Apartados.php

--- controllers/Apartados.php
+++ controllers/Apartados.php
@@ -183,7 +183,7 @@ class Apartados extends Controller
     {
         $empresa = $this->model->getEmpresa();
         $data['apartado'] = $this->model->getApartado($idApartado);
-        $nombre_impresora = "4BARCODE3B-365B";
+        $nombre_impresora = "4BARCODE 3B-365B";
         $connector = new WindowsPrintConnector($nombre_impresora);
         $printer = new Printer($connector);
 
